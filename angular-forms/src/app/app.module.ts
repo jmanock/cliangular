f8ed3029c0b97a1f99928098e7ed3da254b84824
jManock <jonmanock@gmail.com>
2018-08-14 17:37:52 -0400

    works

--- angular-forms/src/app/app.module.ts
+++ angular-forms/src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroFormComponent
-  ],
-  imports: [
+  imports:[
     BrowserModule,
     FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations:[
+    AppComponent,
+    HeroFormComponent
+  ],
+  providers:[],
+  bootstrap:[ AppComponent ]
 })
 export class AppModule { }
